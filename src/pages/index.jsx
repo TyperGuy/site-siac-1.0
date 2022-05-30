fd2352050490b23330f08188d90b33e31f15cb7a
TyperGuy <ray.developper@gmail.com>
2022-05-30 15:16:42 +0100

    feat: remove messanger chat component

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -4,23 +4,17 @@ import Partners from '../views/partners/Partners.jsx';
 import Floating from '../components/floating-button/Floating.jsx'
 import Benefic from '../views/benefic/Benefic.jsx';
 import Stats from '../views/stats/Stats.jsx';
-import {Flex} from '@chakra-ui/react';
-import MessengerCustomerChat from 'react-messenger-customer-chat';
 
 export default function Home() {
   return (
-    <Flex width="100%" direction="column"  align="center">
+    <div style={{display: 'flex', alignItems: 'center' ,flexDirection:'column', position:'relative'}}>
       <Grid/>
       <Benefic/>
       <Fam/>
       <Stats/>
       <Partners/>
       <Floating/>
-      <MessengerCustomerChat
-        pageId="108266877196447"
-        appId="631180188161263"
-      />
-    </Flex>
+    </div>
   )
 }
 
--- src/views/grid-on-header/Grid.jsx
+++ src/views/grid-on-header/Grid.jsx
@@ -1,7 +1,5 @@
 import React from 'react';
-import {Flex} from '@chakra-ui/react';
 import Style from './Grid.module.css';
-import Publicity from '../../components/publicity/Publicity.jsx'
 import News from './components/news/News.jsx';
 import Services from './components/siac-services/Services.jsx';
 
@@ -12,7 +10,6 @@ function Grid(){
           <Services/>
           <News/>
         </div>
-        <Publicity/>
       </> 
       )
  
