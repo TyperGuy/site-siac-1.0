84d85c30daa833d275644368f01bb800646ee63c
TyperGuy <ray.developper@gmail.com>
2022-06-10 11:45:16 +0100

    feat : update fqa component

--- src/pages/FAQ.jsx
+++ src/pages/FAQ.jsx
@@ -8,8 +8,15 @@ function FAQ () {
   return(
     <Flex width="vw" justify="center" align="center" direction="column">
       <Image src={img} marginTop="20px" width="40vw"  alt="Citizens Image"/>
-      <Flex width="50vw" direction="column">
-        <hr/>
+      <Flex 
+        width="50vw" 
+        direction="column" 
+        style={{
+          padding:'2rem',
+          margin:'2rem 0',
+          boxShadow:' rgba(99, 99, 99, 0.2) 0px 2px 8px 0px',
+          borderRadius:'.5rem'
+        }} >
         <Flex flexWrap="wrap" gap="4" justify="space-between"  direction="column">
           <p style={{marginTop:25, marginBottom:10,fontWeight:500,fontSize:"1.5rem", color:"#abaeb0"}}>Perguntas mais Frequêntes</p>
           <InputGroup>
